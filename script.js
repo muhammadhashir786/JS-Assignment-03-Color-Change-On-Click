8c9b0b8378fc0f4aca1a4ab076ef8a517c4f0869
Muhammad Hashir <muhammadhashiransari12@gmail.com>
2026-06-21 23:21:05 +0500

    Add

---
+++ script.js
@@ -0,0 +1,19 @@
+function colorRed() {
+    document.body.style.backgroundColor = "#d20000";
+}
+
+function colorBlue() {
+    document.body.style.backgroundColor = "rgb(0, 0, 217)";
+}
+
+function colorGreen() {
+    document.body.style.backgroundColor = "#38df00";
+}
+
+function colorOrange() {
+    document.body.style.backgroundColor = "rgb(255, 166, 0)";
+}
+
+function resetColor() {
+    document.body.style.backgroundColor = "white";
+}
